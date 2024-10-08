39082686107c5309c025a9eacec24dddbfff875e
Matheus Henrique Silva Coelho <62672185+matheushrq@users.noreply.github.com>
2024-10-08 15:35:21 -0300

    Trigger para validação de alterações em tabela
    Trigger criada para validação de alterações na tabela auditoria_trigger deixando sempre a data de alteração sendo o dia atual, que anteriormente estava nula.

---
+++ triggers/trigger_update.sql
@@ -0,0 +1,23 @@
+create table dbo.auditoria_trigger
+(
+	id int identity primary key,
+	dt_alteracao date,
+	status_alteracao varchar(30)
+)
+
+insert	into dbo.auditoria_trigger (dt_alteracao, status_alteracao) values (null, 'Pendente')
+insert	into dbo.auditoria_trigger (dt_alteracao, status_alteracao) values (null, 'Pendente')
+insert	into dbo.auditoria_trigger (dt_alteracao, status_alteracao) values (null, 'Pendente')
+
+create or alter trigger dbo.validacao_alteracao on dbo.auditoria_trigger
+after update
+as
+begin
+	set nocount on
+	update atr set atr.dt_alteracao = getdate()
+	from dbo.auditoria_trigger atr
+	join inserted i
+	on i.id = atr.id
+	and i.status_alteracao = 'Aprovado'
+end
+go
