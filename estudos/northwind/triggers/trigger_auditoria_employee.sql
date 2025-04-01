3f6a8072ee95365e1d4f623e37625623f3189438
Matheus Henrique Silva Coelho <62672185+matheushrq@users.noreply.github.com>
2025-04-01 20:46:20 -0300

    Estudo
    Trigger desenvolvida para auditoria com o banco de dados Northwind.

---
+++ estudos/northwind/triggers/trigger_auditoria_employee.sql
@@ -0,0 +1,57 @@
+use northwind
+go
+
+create table dbo.employee_auditoria
+(
+	id int identity primary key,
+	employeeid int not null,
+	lastname nvarchar(40),
+	firstname nvarchar(20),
+	title nvarchar(60),
+	titleofcourtesy nvarchar(50),
+	birthdate date,
+	updatedat date not null,
+	operation char(3) not null,
+	check(operation = 'INS' or operation = 'DEL')
+)
+
+create or alter trigger dbo.trg_insert_employee
+on dbo.employees
+after insert, delete
+as begin
+	set nocount on
+	insert into dbo.employee_auditoria(
+		employeeid,
+		lastname,
+		firstname,
+		title,
+		titleofcourtesy,
+		birthdate,
+		updatedat,
+		operation
+	)
+	select
+		i.EmployeeID,
+		LastName,
+		FirstName,
+		Title,
+		TitleOfCourtesy,
+		BirthDate,
+		getdate(),
+		'INS'
+	from inserted i
+	union all
+	select
+		d.EmployeeID,
+		LastName,
+		FirstName,
+		Title,
+		TitleOfCourtesy,
+		BirthDate,
+		getdate(),
+		'DEL'
+	from deleted d
+end
+go
+
+alter table dbo.employees enable trigger trg_insert_employee
