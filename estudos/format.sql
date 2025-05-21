9a81009b95bcbd3dda2d03a005fdce9d08f2a7f3
Matheus Henrique <henriquem320@gmail.com>
2025-05-21 19:57:35 -0300

    main: Estudos com FORMAT.

--- estudos/format.sql
+++ estudos/format.sql
@@ -11,12 +11,16 @@ select	format(@data_teste, 'D', 'pt-BR') -- 'D': traz a data no formato brasilei
 SELECT	FORMAT(CAST('07:35' AS TIME), N'hh\.mm'); --> returns 07.35
 SELECT	FORMAT(CAST('07:35' AS TIME), N'hh\:mm'); --> returns 07:35
 
-
-
-declare @data_exp datetime = '20251231'
-
+/*
 select	convert(smalldatetime, FORMAT(DATEADD(month, 7, @data_exp), 'yyyyMM' + '21')) -- altera a data para 21/07/2026
 select	convert(smalldatetime, convert(varchar(8), dateadd(month, 1, @data_exp), 121) + '30') -- altera a data para 30/01/2026
 
 select	convert(smalldatetime, FORMAT(DATEADD(month, 0, GETDATE()), 'yyyyMM' + '10')) -- não altera o mês e muda o dia para 10
 select	convert(smalldatetime, convert(varchar(8), dateadd(month, 1, GETDATE()), 121) + '30') -- altera a data para 30/06/2026
+*/
+
+declare @data_exp	datetime = '20251231',
+		@d			int = 15
+
+select	convert(smalldatetime, format(@data_exp, 'yyyyMM' + RIGHT(convert(varchar(8), day(getdate()), 103), 2)))
+select	convert(smalldatetime, format(@data_exp, 'yyyyMM' + RIGHT('0' + convert(varchar(10), @d), 2)))
