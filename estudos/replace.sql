017e847ca5d4466953648cebfe2cd3c5febf3f83
Matheus Henrique <henriquem320@gmail.com>
2025-10-15 21:23:37 -0300

    main: Ajuste de encoding.

--- estudos/replace.sql
+++ estudos/replace.sql
@@ -1,5 +1,5 @@
 declare 
-	@str varchar(max) = 'Esse é um teste com replace'
+	@str varchar(max) = 'Esse Ã© um teste com replace'
 
 select	@str
 select	replace(@str, 'replace', 'replace no sql server')
