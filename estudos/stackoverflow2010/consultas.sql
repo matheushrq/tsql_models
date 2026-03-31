12e10c16e6bc15465b0ae0d83f196a862eb90e76
Matheus Henrique <henriquem320@gmail.com>
2026-03-31 16:56:19 -0300

    main: Tratamento de dados.

--- estudos/stackoverflow2010/consultas.sql
+++ estudos/stackoverflow2010/consultas.sql
@@ -2,21 +2,31 @@ with teste as (
 	select	top 100
 			b.name,
 			convert(varchar(11), b.date, 103) [date],
-			replace(u.AboutMe, '', 'N/A') AboutMe,
+			replace(isnull(u.AboutMe, 'Lorem ipsum dolor sit amet, consectetur adipiscing elit...'), '', 'N/A') AboutMe,
 			isnull(u.Age, datediff(year, CreationDate, getdate())) age,
 			convert(varchar(10), u.CreationDate, 103) CreationDate,
 			upper(left(u.DisplayName, 1)) + lower(substring(u.DisplayName, 2, len(u.DisplayName))) displayName, -- primeira letra maiúscula e o restante minúsculo
 			u.DownVotes,
 			convert(varchar(10), u.LastAccessDate, 103) LastAccessDate,
-			u.Location,
+			replace(isnull(u.Location, 'N/A'), '', 'N/A') Location,
 			u.Reputation,
 			u.UpVotes,
-			u.Views
+			u.Views,
+			soma_votos = u.UpVotes + u.DownVotes
 	from	Badges b
 	join	Users u
 	on		u.Id = b.UserId
 )
 
-select	*
+select	ROW_NUMBER() over (order by soma_votos desc) as id_seq,
+		*
 from	teste
-order	by DownVotes desc
+order	by soma_votos desc
+
+return
+
+select	upper(left(name, 1)) + lower(SUBSTRING(name, 2, len(name))) name,
+		count(*) qtd
+from	Badges
+group	by name
+order	by name
